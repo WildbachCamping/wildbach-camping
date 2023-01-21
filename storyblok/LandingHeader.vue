9eea1b8bf0223fb70267ca62d7fd357ce4df3a31
Laurenz Saam <laurenz.saam@gmail.com>
2023-01-21 15:10:50 +0100

    headers

--- storyblok/LandingHeader.vue
+++ storyblok/LandingHeader.vue
@@ -12,12 +12,12 @@ const props = defineProps({
     <section class="container min-h-[60vh] pt-10 flex flex-col justify-center items-center" id="welcome">
 
         <div class="text-center" data-aos="fade-up" data-aos-duration="1000" data-aos-offset="500">
-            <h1 class=" font-bold mb-4">{{ blok.title }}</h1>
+            <h2 class=" font-bold text-3xl md:text-4xl md:mb-4">{{ blok.title }}</h2>
 
         </div>
-        <div class="text-center pt-6" data-aos="fade-right" data-aos-delay="700" data-aos-offset="500"
+        <div class="text-center pt-2 md:pt-6" data-aos="fade-right" data-aos-delay="700" data-aos-offset="500"
             data-aos-duration="2000">
-            <h1 class="font-bold mb-4">{{ blok.subtitle }}</h1>
+            <h2 class="font-bold text-3xl md:text-4xl mb-4">{{ blok.subtitle }}</h2>
 
         </div>
     </section>
