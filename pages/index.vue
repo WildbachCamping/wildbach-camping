ed41ab4abf5c1769dfe4ca14185a40887ccb6208
Laurenz Saam <laurenz.saam@gmail.com>
2023-01-21 14:07:05 +0100

    updates

--- pages/index.vue
+++ pages/index.vue
@@ -9,12 +9,12 @@
         <div class="container flex flex-col justify-center gap-6 items-center mt-28">
             <h2>Möchtest du mehr erfahren?</h2>
             <div class="container flex flex-col lg:flex-row justify-center gap-6 items-center mt-8">
-                <div class="flex flex-col items-center">
+                <div class="flex flex-col items-center md:pt-12">
                     <h2>Camping</h2>
                     <img src="@/assets/images/home/camping.jpeg"
                         class="h-80 rounded-md object-cover w-full md:w-80 mt-4" alt="">
                     <button type="button"
-                        class="rounded-lg w-1/2 mt-6 bg-gray-800/90 px-4 py-1 hover:bg-gray-800/70 duration-300">
+                        class="rounded-lg w-1/2 mt-6 bg-secondary-green/90 px-4 py-1 hover:bg-secondary-green/70 duration-300">
                         <NuxtLink class="text-white" to="/camping">
                             Mehr Infos
                         </NuxtLink>
@@ -25,7 +25,7 @@
                     <img src="@/assets/images/home/mietobjekt.jpeg"
                         class="h-80 rounded-md object-cover w-full md:w-80 mt-4" alt="">
                     <button type="button"
-                        class="rounded-lg w-1/2 mt-6 bg-gray-800/90 px-4 py-1 hover:bg-gray-800/70 duration-300">
+                        class="rounded-lg w-1/2 mt-6 bg-secondary-green/90 px-4 py-1 hover:bg-secondary-green/70 duration-300">
                         <NuxtLink class="text-white" to="more/mietobjekte">
                             Mehr Infos
                         </NuxtLink>
@@ -36,7 +36,7 @@
                     <img src="@/assets/images/home/dauercamping.jpeg"
                         class="h-80 rounded-md object-cover w-full md:w-80 mt-4" alt="">
                     <button type="button"
-                        class="rounded-lg w-1/2 mt-6 bg-gray-800/90 px-4 py-1 hover:bg-gray-800/70 duration-300">
+                        class="rounded-lg w-1/2 mt-6 bg-secondary-green/90 px-4 py-1 hover:bg-secondary-green/70 duration-300">
                         <NuxtLink class="text-white" to="more/dauercamping">
                             Mehr Infos
                         </NuxtLink>
@@ -46,7 +46,7 @@
         </div>
         <div class="container mt-20 flex justify-center">
             <button type="button"
-                class="rounded-md w-7/8 md:w-1/2 h-20 bg-secondary-green px-4 py-1 hover:bg-gray-800/70 duration-300">
+                class="rounded-md w-7/8 md:w-1/2 h-20 bg-primary-green px-4 py-1 hover:bg-secondary-green/70 duration-300">
                 <NuxtLink class="text-white text-3xl" to="https://wildbach-camping.camping.care/" target="_blank">Zum
                     Buchungsportal</NuxtLink>
             </button>
--- storyblok/Grid.vue
+++ storyblok/Grid.vue
@@ -8,7 +8,8 @@ const props = defineProps({
 </script>
 <template>
     <div class="container">
-        <ul class="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <ul class="grid grid-cols-1 md:grid-cols-2 gap-6" data-aos="fade-up" data-aos-duration="2500"
+            data-aos-delay="500">
             <li v-for="column in blok.columns">
                 <StoryblokComponent :blok="column" />
             </li>
