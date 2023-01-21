ca71f19c1eedd0328878d4006b8ae671b219641d
Laurenz Saam <laurenz.saam@gmail.com>
2023-01-21 14:55:48 +0100

    header

--- storyblok/LandingHeader.vue
+++ storyblok/LandingHeader.vue
@@ -17,7 +17,7 @@ const props = defineProps({
         </div>
         <div class="text-center pt-6" data-aos="fade-right" data-aos-delay="700" data-aos-offset="500"
             data-aos-duration="2000">
-            <h1 class=" mb-4">{{ blok.subtitle }}</h1>
+            <h1 class="font-bold mb-4">{{ blok.subtitle }}</h1>
 
         </div>
     </section>
