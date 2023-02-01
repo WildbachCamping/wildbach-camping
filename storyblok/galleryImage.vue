2bb830d2d4b14607b34d0348c280b17e28fdf3a8
Laurenz Saam <laurenz.saam@gmail.com>
2023-02-01 10:06:22 +0100

    update

--- storyblok/galleryImage.vue
+++ storyblok/galleryImage.vue
@@ -16,7 +16,7 @@ const props = defineProps({
 
     <div class="pt-20 flex flex-col justify-center items-center">
         <img :src="blok.image?.filename" alt=""
-            :class="['object-cover w-full rounded-md shadow-md max-h-[60vh]', thumbnail ? 'max-h-[20vh] ' : '']">
+            :class="['object-cover w-full rounded-md shadow-md max-h-[75vh]', thumbnail ? 'max-h-[25vh] min-h-[25vh]' : '']">
         <p v-if="visible == true" class="mt-4">{{ blok.image.title }}</p>
     </div>
 
