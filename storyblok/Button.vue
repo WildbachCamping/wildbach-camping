eab7b4a0775c16fbd24b79594593dcc4d9f70102
Laurenz Saam <laurenz.saam@gmail.com>
2023-02-04 16:12:36 +0100

    updates

--- storyblok/Button.vue
+++ storyblok/Button.vue
@@ -1,18 +1,16 @@
 <script setup>
 const props = defineProps({
-    blok: {
-        type: Object,
-        default: () => ({}),
-    },
+  blok: {
+    type: Object,
+    default: () => ({}),
+  },
 });
 </script>
 <template>
-  <div>
-    <button
-      type="button"
-      class="rounded-full bg-gray-800/90 px-4 py-1 hover:bg-gray-800/70 duration-300"
-    >
-      <NuxtLink class="text-white" :to="blok.link">{{ blok.label }}</NuxtLink>
+  <div class="container flex flex-col items-center pt-10">
+    <button type="button" class="rounded-full bg-gray-800/90 px-4 py-1 hover:bg-gray-800/70 duration-300 mt-10">
+      <NuxtLink class="text-white flex gap-2" :to="blok.pdf?.filename" target="_blank">{{ blok.label }} <img
+          src="@/assets/images/open-outline.svg" class="w-5" alt=""></NuxtLink>
     </button>
   </div>
 </template>
--- storyblok/FullPageImage.vue
+++ storyblok/FullPageImage.vue
@@ -13,7 +13,8 @@ const props = defineProps({
         <h2 class="text-4xl md:text-5xl mb-4 md:mb-10">{{ blok.title }}</h2>
         <img :src="blok.image[0]?.filename" alt="" class="shadow-md">
 
-        <button type="button" class="rounded-full bg-gray-800/90 px-4 py-1 hover:bg-gray-800/70 duration-300 mt-10">
+        <button type="button" class="rounded-full bg-gray-800/90 px-4 py-1 hover:bg-gray-800/70 duration-300 mt-10"
+            v-if="blok.label">
             <NuxtLink class="text-white flex gap-2" :to="blok.pdf?.filename" target="_blank">{{ blok.label }} <img
                     src="@/assets/images/open-outline.svg" class="w-5" alt=""></NuxtLink>
         </button>
