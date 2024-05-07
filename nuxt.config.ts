28f122ad0939e0ae9c6ccf1ae001b9d6f42141a0
Laurenz Saam <laurenz.saam@example.com>
2024-05-07 18:31:17 +0200

    Update name in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,7 +33,7 @@ export default defineNuxtConfig({
 
     description:
       'Wildbach Camping in Hellenthal am Nationalpark Eifel I Familienfreundliche und entspannte Atmosphäre I Naturbelassen am Bach mit Feuerstellen I Auszeit und Urlaub in der Natur"',
-    name: "Dein Campingplatz am Nationalpark Eifel",
+    name: "Campingplatz in der Eifel: Wildbach Camping",
   },
   sitemap: {
     // sources: ["/api/__sitemap__/urls"],
